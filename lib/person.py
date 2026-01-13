28e45fb9794fa626fd8e1fdec22264523d341283
Brian-melita <brianmelitah@gmail.com>
2026-01-13 16:54:02 +0300

    Final

--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,34 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Admin"):
+        self.name = name
+        self.job = job
+
+    # Name property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = "Unknown"  # fallback default
+
+    # Job property
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = "Admin"  # fallback default
